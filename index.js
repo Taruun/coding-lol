db2a6a922ce56ad8af33c6e805a9d218dca97339
Taruun <phantmetic@gmail.com>
2023-10-03 17:45:23 +0530

    refactor code

--- index.js
+++ index.js
@@ -9,27 +9,46 @@
 //     })
 // }
 
+// const url = "https://backend-omega-seven.vercel.app/api/getjoke"
+// const jokeQuestion = document.querySelector("#p1")
+// const jokePunchline = document.querySelector("#p2")
 
+// const showJoke  = () =>{
 
-const url = "https://backend-omega-seven.vercel.app/api/getjoke"
-const jokeQuestion = document.querySelector("#p1")
-const jokePunchline = document.querySelector("#p2")
+//     const response = fetch(url)
+//     response.then((res)=>{
+//         const json = res.json()
+//         json.then((data)=> {
+//             jokeQuestion.textContent = "Question: " + data[0].question
+//             jokePunchline.textContent = "Answer: " + data[0].punchline
+//         }).catch((err) => {
+//             const errorMessageCreate = document.createElement("h5");
+//             errorMessageCreate.textContent = "Api must be tired :)";
+//             jokeQuestion.append(errorMessageCreate);
+//         });
 
+//     })
+// }
 
+const url = "https://backend-omega-seven.vercel.app/api/getjoke";
+const questions = document.querySelector("#p1");
+const answer = document.querySelector("#p2");
 
-const showJoke  = () =>{
-  
-    const response = fetch(url)
-    response.then((res)=>{
-        const json = res.json()
-        json.then((data)=> {
-            jokeQuestion.textContent = "Question: " + data[0].question
-            jokePunchline.textContent = "Answer: " + data[0].punchline
-        }).catch((err) => {
-            const errorMessageCreate = document.createElement("h5");
-            errorMessageCreate.textContent = "Api must be tired :)";
-            jokeQuestion.append(errorMessageCreate);
-        });
-       
-    })
-}
+const fetchJoke = async () => {
+  const response = await fetch(url);
+  const json = await response.json();
+  const data = json[0];
+  questions.textContent = "Question: " + data.question;
+  answer.textContent = "Answer: " + data.punchline;
+};
+
+const showJokes = async () => {
+  try {
+    await fetchJoke();
+  } catch (err) {
+    const errorMessage = document.createElement("h6");
+    errorMessage.textContent = "Api must be tired :)";
+    jokeQuestion.append(errorMessage);
+  }
+};
+showJokes();
